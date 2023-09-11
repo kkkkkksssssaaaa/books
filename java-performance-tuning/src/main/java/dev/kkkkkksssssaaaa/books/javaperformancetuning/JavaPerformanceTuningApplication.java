54be4fcefddf7b7fce2e5ac194708b3edb641e71
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-11 22:48:40 +0900

    feat: project init

---
+++ java-performance-tuning/src/main/java/dev/kkkkkksssssaaaa/books/javaperformancetuning/JavaPerformanceTuningApplication.java
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.books.javaperformancetuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPerformanceTuningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPerformanceTuningApplication.class, args);
+    }
+
+}
---
+++ java-performance-tuning/src/test/java/dev/kkkkkksssssaaaa/books/javaperformancetuning/JavaPerformanceTuningApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.books.javaperformancetuning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPerformanceTuningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
