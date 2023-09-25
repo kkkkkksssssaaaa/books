89cf02abb509d681631e72cb1d34fbb56987e80f
kkkkkksssssaaaa <kofc312@naver.com>
2023-09-25 23:02:19 +0900

    test: remove @SpringBootTest

--- java-performance-tuning/src/test/java/dev/kkkkkksssssaaaa/books/javaperformancetuning/JavaPerformanceTuningApplicationTests.java
+++ java-performance-tuning/src/test/java/dev/kkkkkksssssaaaa/books/javaperformancetuning/JavaPerformanceTuningApplicationTests.java
@@ -1,13 +1,10 @@
 package dev.kkkkkksssssaaaa.books.javaperformancetuning;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class JavaPerformanceTuningApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
