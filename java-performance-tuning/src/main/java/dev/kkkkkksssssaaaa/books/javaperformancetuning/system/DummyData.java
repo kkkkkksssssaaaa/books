e495988d427796e54b607475ea8c8cb7c579473b
kkkkkksssssaaaa <kofc312@naver.com>
2024-01-17 22:50:30 +0900

    refactor: cleanup directory

---
+++ java-performance-tuning/src/main/java/dev/kkkkkksssssaaaa/books/javaperformancetuning/system/DummyData.java
@@ -0,0 +1,15 @@
+package dev.kkkkkksssssaaaa.books.javaperformancetuning.system;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DummyData {
+
+    HashMap<String, String> map;
+    ArrayList<String> list;
+
+    public DummyData(HashMap<String, String> map, ArrayList<String> list) {
+        this.map = map;
+        this.list = list;
+    }
+}
