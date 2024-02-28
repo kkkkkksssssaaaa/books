012dd869ef3a7ae07e72eab1fd5e561680d2d2f0
kkkkkksssssaaaa <kofc312@naver.com>
2024-02-28 23:29:23 +0900

    refactor: remove spring annotation on main class

--- atomickotlin/src/main/kotlin/dev/kkkkkksssssaaaa/books/atomickotlin/AtomickotlinApplication.kt
+++ atomickotlin/src/main/kotlin/dev/kkkkkksssssaaaa/books/atomickotlin/AtomickotlinApplication.kt
@@ -1,11 +1,6 @@
 package dev.kkkkkksssssaaaa.books.atomickotlin
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
 class AtomickotlinApplication
 
 fun main(args: Array<String>) {
-    runApplication<AtomickotlinApplication>(*args)
 }
