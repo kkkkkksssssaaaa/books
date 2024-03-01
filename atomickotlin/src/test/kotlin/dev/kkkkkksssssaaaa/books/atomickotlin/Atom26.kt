ff5af927ec4172ca486ad08463bd578ce2e12b83
kkkkkksssssaaaa <kofc312@naver.com>
2024-03-01 23:57:29 +0900

    test(Atom26): add more test

--- atomickotlin/src/test/kotlin/dev/kkkkkksssssaaaa/books/atomickotlin/Atom26.kt
+++ atomickotlin/src/test/kotlin/dev/kkkkkksssssaaaa/books/atomickotlin/Atom26.kt
@@ -15,5 +15,30 @@ class Atom26 {
         // 요소가 포함 되어 있는지 검사
         assertTrue(1 in intSet)
         assertTrue(intSet.contains(1))
+
+        // 합집합
+        // {set1}.union({set2} 의 결과는 set1 에 set2 의 요소를 더한 결과를 반환함
+        assertEquals(
+            intSet.union(setOf(3, 4, 5, 6)),
+            setOf(1, 2, 3, 4, 5, 6, 9)
+        )
+
+        // 교집합
+        assertEquals(
+            intSet intersect setOf(0, 1, 2, 7, 8),
+            setOf(1, 2)
+        )
+
+        // 차집합1
+        assertEquals(
+            intSet subtract setOf(0, 1, 9, 10),
+            setOf(2, 3, 4)
+        )
+
+        // 차집합2, - 기호로도 사용할 수 있다
+        assertEquals(
+            intSet - setOf(0, 1, 9, 10),
+            setOf(2, 3, 4)
+        )
     }
 }
