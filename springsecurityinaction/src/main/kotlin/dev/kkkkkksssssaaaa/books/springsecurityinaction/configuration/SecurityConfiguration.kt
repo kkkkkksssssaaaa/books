22126ff775edf8a1cb22a2f5aca066d286a8adaa
kkkkkksssssaaaa <kofc312@naver.com>
2024-03-23 23:58:48 +0900

    feat: override UserDetailsService, PasswordEncoder beans

---
+++ springsecurityinaction/src/main/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/configuration/SecurityConfiguration.kt
@@ -0,0 +1,32 @@
+package dev.kkkkkksssssaaaa.books.springsecurityinaction.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+
+@Configuration
+class SecurityConfiguration {
+    @Bean
+    fun userDetailsService(): UserDetailsService {
+        val userDetailsService = InMemoryUserDetailsManager()
+
+        val user: UserDetails = User.withUsername("kkkkkksssssaaaa")
+            .password("kkkkkksssssaaaa")
+            .authorities("read")
+            .build()
+
+        userDetailsService.createUser(user)
+
+        return userDetailsService
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return NoOpPasswordEncoder.getInstance()
+    }
+}
