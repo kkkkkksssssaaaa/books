2622f3e43b3a32d51d30ff52ed97df4c382ca412
kkkkkksssssaaaa <kofc312@naver.com>
2024-03-23 23:33:49 +0900

    feat: add hello controller

---
+++ springsecurityinaction/src/main/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/controller/HelloController.kt
@@ -0,0 +1,12 @@
+package dev.kkkkkksssssaaaa.books.springsecurityinaction.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello!"
+    }
+}
