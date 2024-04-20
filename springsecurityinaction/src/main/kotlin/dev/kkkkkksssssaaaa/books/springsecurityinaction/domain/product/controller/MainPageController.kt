92b172ecf6c12bb483bfe2aa68e63f7dcb0fa230
kkkkkksssssaaaa <kofc312@naver.com>
2024-04-21 00:05:08 +0900

    feat: add hidden csrf token field to main page

--- springsecurityinaction/src/main/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/domain/product/controller/MainPageController.kt
+++ springsecurityinaction/src/main/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/domain/product/controller/MainPageController.kt
@@ -1,15 +1,20 @@
 package dev.kkkkkksssssaaaa.books.springsecurityinaction.domain.product.controller
 
 import dev.kkkkkksssssaaaa.books.springsecurityinaction.domain.product.service.ProductService
+import org.slf4j.LoggerFactory
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class MainPageController(
     private val productService: ProductService
 ) {
+    private val log = LoggerFactory.getLogger(MainPageController::class.java)!!
+
     @GetMapping("/main")
     fun main(
         authentication: Authentication,
@@ -17,6 +22,13 @@ class MainPageController(
     ): String {
         model.addAttribute("username", authentication.name)
         model.addAttribute("products", productService.findAll())
+
+        return "main"
+    }
+
+    @PostMapping("/product")
+    fun addProduct(@RequestParam name: String): String {
+        log.info("Adding product $name")
 
         return "main"
     }
