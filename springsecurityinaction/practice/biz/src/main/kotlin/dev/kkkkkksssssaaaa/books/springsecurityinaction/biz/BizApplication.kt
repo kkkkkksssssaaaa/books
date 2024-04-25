0ac3e822f4b9d348042ffb3e9b765c9925955442
kkkkkksssssaaaa <kofc312@naver.com>
2024-04-25 22:35:41 +0900

    feat: add biz server module

---
+++ springsecurityinaction/practice/biz/src/main/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/biz/BizApplication.kt
@@ -0,0 +1,11 @@
+package dev.kkkkkksssssaaaa.books.springsecurityinaction.biz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BizApplication
+
+fun main(args: Array<String>) {
+	runApplication<BizApplication>(*args)
+}
---
+++ springsecurityinaction/practice/biz/src/test/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/biz/BizApplicationTests.kt
@@ -0,0 +1,11 @@
+package dev.kkkkkksssssaaaa.books.springsecurityinaction.biz
+
+import org.junit.jupiter.api.Test
+
+class BizApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
