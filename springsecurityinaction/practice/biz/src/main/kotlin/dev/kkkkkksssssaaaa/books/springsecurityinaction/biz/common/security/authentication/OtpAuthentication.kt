d3f3cc713795b5651e9e13e6d5ec66ff70ae93bc
kkkkkksssssaaaa <kofc312@naver.com>
2024-04-25 23:06:18 +0900

    feat: add authentication providers

---
+++ springsecurityinaction/practice/biz/src/main/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/biz/common/security/authentication/OtpAuthentication.kt
@@ -0,0 +1,17 @@
+package dev.kkkkkksssssaaaa.books.springsecurityinaction.biz.common.security.authentication
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.GrantedAuthority
+
+class OtpAuthentication: UsernamePasswordAuthenticationToken {
+    constructor(
+        principal: Any,
+        credentials: Any
+    ) : super(principal, credentials)
+
+    constructor(
+        principal: Any,
+        credentials: Any,
+        authorities: MutableCollection<out GrantedAuthority>
+    ) : super(principal, credentials, authorities)
+}
---
+++ springsecurityinaction/practice/biz/src/main/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/biz/common/security/provider/OtpAuthenticationProvider.kt
@@ -0,0 +1,31 @@
+package dev.kkkkkksssssaaaa.books.springsecurityinaction.biz.common.security.provider
+
+import dev.kkkkkksssssaaaa.books.springsecurityinaction.biz.common.proxy.AuthenticationServerProxy
+import dev.kkkkkksssssaaaa.books.springsecurityinaction.biz.common.security.authentication.OtpAuthentication
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.core.Authentication
+
+class OtpAuthenticationProvider(
+    private val proxy: AuthenticationServerProxy
+): AuthenticationProvider {
+    override fun authenticate(authentication: Authentication): Authentication {
+        val username = authentication.name
+        val otp = authentication.credentials.toString()
+
+        val response = proxy.sendOtp(
+            username = username,
+            code = otp
+        )
+
+        if (response) {
+            return OtpAuthentication(username, otp)
+        } else {
+            throw BadCredentialsException("Bad credentials.")
+        }
+    }
+
+    override fun supports(authentication: Class<*>): Boolean {
+        return OtpAuthentication::class.java.isAssignableFrom(authentication)
+    }
+}
