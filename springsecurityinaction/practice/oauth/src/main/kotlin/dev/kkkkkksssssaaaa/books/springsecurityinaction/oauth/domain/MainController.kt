a8a4a5035097bf966494db0f925f4b81dcc46ba3
kkkkkksssssaaaa <kofc312@naver.com>
2024-05-03 23:44:55 +0900

    feat: add github login

--- springsecurityinaction/practice/oauth/src/main/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/oauth/domain/MainController.kt
+++ springsecurityinaction/practice/oauth/src/main/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/oauth/domain/MainController.kt
@@ -1,12 +1,22 @@
 package dev.kkkkkksssssaaaa.books.springsecurityinaction.oauth.domain
 
+import org.slf4j.LoggerFactory
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class MainController {
+    companion object {
+        private val log = LoggerFactory.getLogger(this::class.java)
+    }
+
     @GetMapping
-    fun main(): String {
+    fun main(
+        token: OAuth2AuthenticationToken
+    ): String {
+        log.info(token.principal.toString())
+
         return "main.html"
     }
 }
