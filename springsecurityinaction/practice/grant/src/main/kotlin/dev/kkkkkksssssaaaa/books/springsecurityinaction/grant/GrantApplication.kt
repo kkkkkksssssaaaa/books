80e3ef91e6e8d4c3d8df30c5c5427cff12e4e1a6
kkkkkksssssaaaa <kofc312@naver.com>
2024-05-23 22:56:27 +0900

    feat: add grant server module

---
+++ springsecurityinaction/practice/grant/src/main/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/grant/GrantApplication.kt
@@ -0,0 +1,11 @@
+package dev.kkkkkksssssaaaa.books.springsecurityinaction.grant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrantApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrantApplication>(*args)
+}
---
+++ springsecurityinaction/practice/grant/src/test/kotlin/dev/kkkkkksssssaaaa/books/springsecurityinaction/grant/GrantApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.books.springsecurityinaction.grant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrantApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
