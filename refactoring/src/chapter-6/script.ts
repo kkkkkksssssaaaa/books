06d436b8c442d53d396a3188ce3108d22d26a7a1
kkkkkksssssaaaa <kofc312@naver.com>
2024-07-01 23:48:21 +0900

    feat(refactoring): complete chapter 6 example-1

--- refactoring/src/chapter-6/script.ts
+++ refactoring/src/chapter-6/script.ts
@@ -1,16 +1,26 @@
 function printOwing(invoice: Invoice) {
-  let outstanding = 0;
+  printBanner();
 
+  const outstanding = calculateOutstanding(invoice);
+
+  recordDueDate(invoice);
+
+  printDetails(invoice, outstanding);
+}
+
+const printBanner = () => {
   console.log("*********************");
   console.log("****** 고객 채무 ******");
   console.log("*********************");
+}
 
-  // 미해결 채무(outstanding) 을 계산한다.
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+const printDetails = (invoice: Invoice, outstanding: number) => {
+  console.log(`고객명: ${ invoice.customer }`);
+  console.log(`채무액: ${ outstanding }`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+}
 
-  // 마감일(dueDate)을 기록한다.
+const recordDueDate = (invoice: Invoice) => {
   const today = Clock.today;
   
   invoice.dueDate = new Date(
@@ -19,11 +29,16 @@ function printOwing(invoice: Invoice) {
     today.getDate(),
     + 30
   );
+}
 
-  // 세부 사항을 출력한다.
-  console.log(`고객명: ${ invoice.customer }`);
-  console.log(`채무액: ${ outstanding }`);
-  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+const calculateOutstanding = (invoice: Invoice) => {
+  let result = 0;
+
+  for (const o of invoice.orders) {
+    result += o.amount;
+  }
+
+  return result;
 }
 
 class Clock {
