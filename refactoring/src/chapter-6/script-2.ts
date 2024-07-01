0a96dc4338c69cacad327e404df3557beda4f99a
kkkkkksssssaaaa <kofc312@naver.com>
2024-07-01 23:58:31 +0900

    feat(refactoring): complate chapter 6 example-2

--- refactoring/src/chapter-6/script-2.ts
+++ refactoring/src/chapter-6/script-2.ts
@@ -1,6 +1,40 @@
-const price = (order) => {
-  // 가격(price) = 기본 가격 - 수량 할인 + 배송비
-  return order.quantity * order.itemPrice
-    - Math.max(0, order.quantity - 500) * order.itemPrice * 0.05
-    + Math.min(order.quantity * order.itemPrice * 0.1, 100);
+interface OrderData {
+  quantity: number;
+  itemPrice: number;
+}
+
+class Order {
+  private _quantity: number;
+  private _itemPrice: number;
+
+  constructor(data: OrderData) {
+    this._quantity = data.quantity;
+    this._itemPrice = data.itemPrice;
+  }
+
+  get quantity() {
+    return this._quantity;
+  }
+
+  get itemPrice() {
+    return this._itemPrice;
+  }
+
+  get price() {
+    return this.basePrice
+    - this.quantityDiscount
+    + this.shipping;
+  }
+
+  get basePrice() {
+    return this._quantity * this._itemPrice;
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, this._quantity - 500) * this._itemPrice * 0.05;
+  }
+
+  get shipping() {
+    return Math.min(this.basePrice * 0.1, 100);
+  }
 }
