d4af441d99c7f277c89c7c3d34ccf0ebc68e8e50
kkkkkksssssaaaa <kofc312@naver.com>
2024-07-01 23:32:54 +0900

    test(refactoring): add more test to province

--- refactoring/src/test/chapter-4/province.spec.ts
+++ refactoring/src/test/chapter-4/province.spec.ts
@@ -1,25 +1,65 @@
 import {describe, expect, test, beforeEach} from "@jest/globals";
-import { Province, sampleProvinceData } from "../../chapter-4/province";
+import { Province, ProvinceData, sampleProvinceData } from "../../chapter-4/province";
 
-describe("province", () => {
-  let asia;
-
-  beforeEach(() => {
-    asia = new Province(sampleProvinceData());
+describe("full test", () => {
+  describe("province", () => {
+    let asia;
+  
+    beforeEach(() => {
+      asia = new Province(sampleProvinceData());
+    });
+  
+    test("shorfall", () => {
+      expect(asia.shortfall).toBe(5);
+    });
+  
+    test("profit", () => {
+      expect(asia.profit).toBe(230);
+    });
+  
+    test("change production", () => {
+      asia.producers[0].production = 20;
+  
+      expect(asia.shortfall).toBe(-6);
+      expect(asia.profit).toBe(292);
+    });
+  
+    test("zero demand", () => {
+      asia.demand = 0;
+  
+      expect(asia.shortfall).toBe(-25);
+      expect(asia.profit).toBe(0);
+    });
+  
+    test("negative demand", () => {
+      asia.demand = -1;
+  
+      expect(asia.shortfall).toBe(-26);
+      expect(asia.profit).toBe(-10);
+    });
   });
-
-  test("shorfall", () => {
-    expect(asia.shortfall).toBe(5);
-  });
-
-  test("profit", () => {
-    expect(asia.profit).toBe(230);
-  });
-
-  test("change production", () => {
-    asia.producers[0].production = 20;
-
-    expect(asia.shortfall).toBe(-6);
-    expect(asia.profit).toBe(292);
+  
+  describe("no producers", () => {
+    let noProducers;
+  
+    beforeEach(() => {
+      const data: ProvinceData = {
+        name: "No producers",
+        producers: [],
+        demand: 30,
+        price: 20
+      };
+  
+      noProducers = new Province(data);
+    });
+  
+    test("shortfall", () => {
+      expect(noProducers.shortfall).toBe(30);
+    });
+  
+    test("profit", () => {
+      expect(noProducers.profit).toBe(0);
+    });
   });
 });
+
