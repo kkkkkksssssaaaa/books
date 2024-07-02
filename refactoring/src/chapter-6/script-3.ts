9c7137510cce6f1fcfacc93090a0d1521053f712
kkkkkksssssaaaa <kofc312@naver.com>
2024-07-03 00:03:16 +0900

    feat(refactoring): complate chapter 6 example-3

--- refactoring/src/chapter-6/script-3.ts
+++ refactoring/src/chapter-6/script-3.ts
@@ -1,12 +1,55 @@
-const priceOrder = (product, quantity, shippingMethod) => {
+const priceOrder = (
+  product: Product,
+  quantity: number,
+  shippingMethod: ShippingMethod
+) => {
+  const priceData: PriceData = calculatePricingData(product, quantity);
+  
+  return applyShipping(priceData, shippingMethod);
+}
+
+const calculatePricingData = (
+  product: Product,
+  quantity: number
+): PriceData => {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0)
     * product.basePrice
     * product.discountRate;
-  const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+
+  return {
+    basePrice: basePrice,
+    quantity: quantity,
+    discount: discount
+  };
+}
+
+const applyShipping = (
+  priceData: PriceData,
+  shippingMethod: ShippingMethod,
+) => {
+  const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
     ? shippingMethod.discountedFee : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
 
   return price;
+}
+
+interface PriceData {
+  basePrice: number;
+  quantity: number;
+  discount: number;
+}
+
+interface Product {
+  basePrice: number;
+  discountThreshold: number;
+  discountRate: number;
+}
+
+interface ShippingMethod {
+  discountThreshold: number;
+  discountedFee: number;
+  feePerCase: number;
 }
